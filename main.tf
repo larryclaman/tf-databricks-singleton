ccc61d5cacdd77ec472b8ed683dad67d34b71256
Larry Claman <25161683+larryclaman@users.noreply.github.com>
2023-02-01 08:43:25 -0500

    removed depends_on

--- main.tf
+++ main.tf
@@ -23,9 +23,7 @@ provider "azurerm" {
 provider "databricks" {
   host                        = azurerm_databricks_workspace.this.workspace_url
   // azure_workspace_resource_id = azurerm_databricks_workspace.this.id
- depends_on = [
-    azurerm_databricks_workspace.this.id
- ]
+ 
 }
 
 
